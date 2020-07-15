29cf0d7b6ab2324c99a85448be487e76ec0d3fb7
Cosmin Tanislav <demonsingur@gmail.com>
2020-07-15 03:52:54 +0200

    power: reset: always preserve memory
    Change-Id: I9fe5d742f899b8825fdfb3c6cba867e0c75422fb
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- drivers/power/reset/msm-poweroff.c
+++ drivers/power/reset/msm-poweroff.c
@@ -306,6 +306,8 @@ static void msm_restart_prepare(const char *cmd)
 	need_warm_reset = true;
 #endif
 
+	need_warm_reset = true;
+
 	/* Hard reset the PMIC unless memory contents must be maintained. */
 	if (need_warm_reset || in_panic)
 		qpnp_pon_system_pwr_off(PON_POWER_OFF_WARM_RESET);
